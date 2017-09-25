f943da82e21ac5ac16fc9f84797ef3a801fc3813
Andrew Greenough <andygreenough@gmail.com>
2017-09-25 23:16:36 +0100

    Creating a DataService Singleton

---
+++ htchhkr/Services/DataService.swift
@@ -0,0 +1,29 @@
+//
+//  DataService.swift
+//  htchhkr
+//
+//  Created by Andrew Greenough on 25/09/2017.
+//  Copyright © 2017 Andrew Greenough. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+    
+    private(set) var REF_BASE = DB_BASE
+    private(set) var REF_USERS = DB_BASE.child("users")
+    private(set) var REF_DRIVERS = DB_BASE.child("drivers")
+    private(set) var REF_TRIPS = DB_BASE.child("trips")
+    
+    func createFirebaseDBUser(uid: String, userData: [String : Any], isDriver: Bool) {
+        if isDriver {
+            REF_DRIVERS.child(uid).updateChildValues(userData)
+        } else {
+            REF_USERS.child(uid).updateChildValues(userData)
+        }
+    }
+}
